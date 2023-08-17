24718daf37847f41f9978b60c0bdde54ac5a98f2
Ankit Parida <114300894+Ankit6989@users.noreply.github.com>
2023-08-17 19:58:10 +0530

    Update build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -15,10 +15,17 @@ buildscript {
 
 // This line declares a Gradle task named "clean." The type: Delete part specifies that the task is of type "Delete," which means it will perform file deletion operations. 
 // This line registers a custom Gradle task named "clean." The task's name is specified as "clean," and it uses the Delete::class class reference to define the task's type.
+
 tasks.register("clean", Delete::class) {
 
 // This line is the action associated with the "clean" task. 
 // It instructs Gradle to delete the "build" directory of the root project. 
 // The "rootProject" is a reference to the main project in the Gradle build, and "buildDir" is a property that represents the build directory for the project.
+
     delete(rootProject.buildDir)
+
+// This code is telling Gradle to create a task called "clean." When you run this task, it will delete a directory called the "build directory."
+// This "build directory" is where all the compiled code and other things generated during the building of your project are stored. 
+// So, running the "clean" task will remove these generated files, giving you a fresh start for your project's next build.
+// It's like cleaning up the mess from previous builds before starting a new one.
 }
